1ded7d84a1c94acda7c2ce5077d64bf2b161dcf9
Julien Banchongphanith <jbanchon@made-f0Ar9s1.clusters.42paris.fr>
2024-09-03 17:16:46 +0200

    new

--- includes/so_long.h
+++ includes/so_long.h
@@ -2,25 +2,39 @@
 # define SO_LONG_H
 
 # include "../mlx/mlx.h"
-# include "libft.h"
 # include "get_next_line.h"
+# include "libft.h"
+# include <X11/keysym.h>
+# include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
-# include <X11/keysym.h>
+
+# define IMG_SIZE 16
 
 typedef struct s_map
 {
-	char **grid;
-	int width;
-	int height;
-	int player_count;
-	int exit_count;
-	int collectible_count;
+	char		**grid;
+	int			width;
+	int			height;
+	int			player_count;
+	int			exit_count;
+	int			collectible_count;
+	int			collected;
+	int			can_exit;
+	int			line_count;
 }				t_map;
 
-typedef struct s_data;
+typedef struct	s_data;
 {
-	void mlx_ptr;
-}
-
+	void *mlx_ptr;
+	void *win_ptr;
+	void *img_ptr;
+	t_map		map;
+	int player_x;
+	int player_y;
+	int count_step;
+	int win_width;
+	int win_height;
+	int img_width;
+	int img_height;
+}				t_data;
--- srcs/init/init_map.c
+++ srcs/init/init_map.c
@@ -6,7 +6,24 @@
 /*   By: jbanchon <jbanchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 15:16:35 by jbanchon          #+#    #+#             */
-/*   Updated: 2024/09/03 15:16:35 by jbanchon         ###   ########.fr       */
+/*   Updated: 2024/09/03 17:12:12 by jbanchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
+
+void	init_map(t_data *data)
+{
+	data->i = 0;
+	data->j = 0;
+	data->map.count_p = 0;
+	data->map.count_e = 0;
+	data->map.count_c = 0;
+	data->map.collected = 0;
+	data->map.can_exit = 0;
+	data->count_step = 0;
+	data->win_height = data->map.line_count * IMG_SIZE;
+	data->win_width = (ft_strlen(data->map.map[0]) - 1) * IMG_SIZE;
+	data->img.height = IMG_SIZE;
+	data->img.width = IMG_SIZE;
+}
